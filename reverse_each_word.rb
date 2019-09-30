4e5db72370a8ac4202012711eabd01791ab08fef
Sydney Werbach <sydwer@gmail.com>
2019-09-30 05:16:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(sentance)
   now_an_array= sentance.split("")
-  semifinal_utterance = now_an_array.each {|word| word.reverse!}
-  final_utterance = semifinal_utterance.join("")
-  return final_utterance
+  reverse_it = now_an_array.each { |word| word.reverse
 end
